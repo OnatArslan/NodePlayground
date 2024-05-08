bc2f88e1ef3e2da256eb2191e293518ffe568d1f
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-05-08 20:20:24 +0300

    Commit fo backend with node (express will be section 3 or 4)

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -25,21 +25,61 @@ const url = require("url");
 // });
 // console.log(`File is reading...`);
 
+// create constant variables and templates
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  `utf-8`
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  `utf-8`
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  `utf-8`
+);
+const dataObjects = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
+  }
+  return output;
+};
+
 // SERVER ____________----------------------______________________
 const server = http.createServer((request, response) => {
   console.log(request.url);
   const pathName = request.url;
+  //   Overview page
   if (pathName === `/` || pathName === `/overview`) {
-    response.end(`This is the overview`);
+    const cardsHtml = dataObjects.map((el) => {
+      return replaceTemplate(tempCard, el);
+    });
+    const output = tempOverview.replace(`{%PRODUCTS_CARDS%}`, cardsHtml);
+    console.log(cardsHtml);
+    response.end(output);
+
+    // products page
   } else if (pathName === `/product`) {
     response.end(`This is the products page`);
+    // api
+  } else if (pathName === `/api`) {
+    response.end(data);
+    // not found
   } else {
     response.writeHead(404);
 
     response.end(`<h1>This page can not be found</h1>`);
   }
-
-  response.end(`Hello from the server`);
 });
 
 server.listen(8000, `127.0.0.1`, () => {
