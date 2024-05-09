6ed389f88fc2f955a7bb9962fb0bfd41d9479822
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-05-09 20:30:31 +0300

    async await tekrari js repoda yapilacak

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,16 +1,56 @@
 const fs = require("fs");
 const superagent = require(`superagent`);
 
-fs.readFile(`./dog.txt`, `utf-8`, (err, data) => {
-  console.log(data);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+// PROMISES
 
-      console.log(res.body.message);
-      fs.writeFile(`dog-image.txt`, res.body.message, `utf-8`, (err) => {
-        console.log(`Random dog image save to file`);
-      });
+const readFilePro = (file) => {
+  // resolve function is when promise succecsfull
+  // reject is when error is occured
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(`I could not find that file`);
+      resolve(data);
     });
-});
+  });
+};
+
+// readFilePro(`${__dirname}/dog.txt`).then((data) => {
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       fs.writeFile(`dog-image.txt`, res.body.message, `utf-8`, (err) => {
+//         console.log(`Random dog image save to file`);
+//       }); // get method returns a promise
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+// fs.readFile(`${__dirname}/dog.txt`, `utf-8`, (err, data) => {
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       fs.writeFile(`dog-image.txt`, res.body.message, `utf-8`, (err) => {
+//         console.log(`Random dog image save to file`);
+//       }); // get method returns a promise
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+// Promises uses when we know that code retrive a data and say to function, hey when you retrive let me know
+
+// async await ------------------------------------
+const getDogPic = async () => {
+  const data = await readFilePro(`${__dirname}/dog.txt`);
+  console.log(`Breead: ${data}`);
+  const res = await superagent.get(
+    `https://dog.ceo/api/breed/${data}/images/random`
+  );
+  await fs.writeFileSync(`dog-image.txt`, res.body.message, `utf-8`);
+  console.log(`Random dog image save to file`);
+};
+
+getDogPic();
