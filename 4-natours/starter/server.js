bf2ad2e0005e1bfcaba352046069679d82008f91
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-05-12 08:59:23 +0300

    Commit for mongoose and mongoDB sec

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,42 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv'); // for config files
 
 dotenv.config({ path: `./config.env` }); // baglantiyi kurduk
 // first set settings and then run server like below
+
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+// Mongoose is a Object Data Modelling libary for MongoDB and Nodejs
+// setting Databese connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+
+  .then(() => {
+    console.log(`DB connection succesfull`);
+  });
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, `A tour must have a name`],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, `A tour must have a price`],
+  },
+});
+
+const Tour = mongoose.model(`Tour`, toursSchema);
+
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
