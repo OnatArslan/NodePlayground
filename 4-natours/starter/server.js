0b6f7ea520396075a308db2ba4a772b798dba5c9
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-05-15 12:15:17 +0300

    Debug controller and routes, Start for Errors section

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -21,7 +21,7 @@ mongoose
 
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
