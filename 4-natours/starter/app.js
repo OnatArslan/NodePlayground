39833f98bed60a113aed3635f8f2b6844fbbe820
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-05-16 21:48:07 +0300

    Commit for custom Error class

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 // importing routers
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+// Import ErrorClass
+const AppError = require(`./utils/appError`);
+const wrongRoute = require(`./controllers/errorController`);
 // create the app
 const app = express();
 
@@ -22,14 +24,6 @@ app.use(express.json()); // adding middleware
 // serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Create own middleware function
-// This function will run each time our app get request
-app.use((req, res, next) => {
-  console.log(`Hello from the middleware function`);
-  // next() function is mandatory
-  next();
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -38,8 +32,15 @@ app.use((req, res, next) => {
 // create tours data
 
 app.use(`/api/v1/tours`, tourRouter); // app.use(route, router) middleware and tourRouter connection
-
 app.use(`/api/v1/users`, userRouter);
+
+// We need put this unhanled request middleware after the original router because when they can't catch (true request) this will catch wrong request
+app.all(`*`, (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404)); // whenewer we give argument to next() function express will understand a error hapening and jump to error handler middleware
+});
+
+// Error handler middleware first parameter is err and then normal middleware function
+app.use(wrongRoute);
 
 // ---------------------------------------------------------------------------------------------
 //   START THE SERVER
